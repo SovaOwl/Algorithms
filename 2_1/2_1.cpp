1d8e563078bfd5143a885608e700dee935ddac13
SovaOwl <sova27755@gmail.com>
2022-08-29 15:09:16 +0300

    First commit

---
+++ 2_1/2_1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int f1 = 1;
+    int f2 = 1;
+    int i = 0;
+    int s = 0;
+    int n = 5;
+
+    while (i < n)
+    {
+        i++;
+        s = f1 + f2;
+        f1 = f2;
+        f2 = s;
+        cout << s << endl;
+    }
+}
