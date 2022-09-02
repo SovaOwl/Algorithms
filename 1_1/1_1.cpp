43e61cb6cee6277b4d375b73f410ae2de098e84e
SovaOwl <106985564+SovaOwl@users.noreply.github.com>
2022-09-02 16:36:34 +0300

    Update 1_1.cpp

--- 1_1/1_1.cpp
+++ 1_1/1_1.cpp
@@ -1,18 +1,19 @@
-﻿#include <iostream>
+#include <iostream>
 
 using namespace std;
 
-int func(int Starting_point, int arr[])
+int func(int Starting_point, int size, int arr[])
 {
-	for (int i = 0; i < Starting_point; i++)
-		if (arr[i] > Starting_point)
-		{
-			int foo = 0;
-			foo += 9 - i;
-			break;
+	int left = 0;
+	int right = size - 1;
 
-			return foo;
-		}
+	while (left <= right)
+	{
+		int middle = (left + right) / 2;
+		if (arr[middle] > Starting_point) right = middle - 1;
+		else if (arr[middle] <= Starting_point) left = middle + 1;
+	}
+	return size - left;
 }
 
 int main()
@@ -20,10 +21,11 @@ int main()
 	setlocale(LC_ALL, "Rus");
 
 	int Starting_point = -999;
-	int arr[9]{ 14, 16, 19, 32, 32, 32, 56, 69, 72 };
+	int arr[]{ 14, 16, 19, 32, 32, 32, 56, 69, 72 };
+	int size = sizeof(arr) / sizeof(arr[0]);
 
 	cout << "Введите точку отсчёта: ";
 	cin >> Starting_point;
 
-	cout << "Количество элементов в массиве больших, чем " << Starting_point << ": " << func(Starting_point, arr);
+	cout << "Количество элементов в массиве больших, чем " << Starting_point << ": " << func(Starting_point, size, arr);
 }
